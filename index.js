3843c4d2e310ff5acdc7c936d8c749fb0414a4b4
marius220699 <marius.schairer@hfg-gmuend.de>
2019-11-26 11:38:32 +0100

    api

--- index.js
+++ index.js
@@ -37,3 +37,55 @@ if (args.length < 3) {
   originalCurrency = args[1];
   targetCurrency = args[2];
 }
+let output;
+
+const currencies = {
+  EUR: {rate:1, symbol:'€'},
+  USD: {rate:1.107, symbol:'$'}, 
+  CAD: {rate:1.4616, symbol: 'C$'}, //Canadischer Dollar
+  IDR: {rate:15600.62, symbol: 'Rp'}, //Indonesische Rupiah Rp
+  AFN: {rate:87.41, symbol: '؋'},//Afghanischer Afghani ؋
+  EGP: {rate:17.89, symbol: 'E£'}, //Ägypticher Pfund 
+  BOB: {rate:7.74, symbol:'Bs'} //Bolivianischer Boliviano
+
+}
+
+
+
+const amountInEUR = amount / currencies[originalCurrency].rate;
+
+output = amountInEUR * currencies[targetCurrency].rate;
+
+console.log('Der Wert ist', output + currencies[targetCurrency].symbol)
+
+const request = require('request');
+
+request('https://api.exchangeratesapi.io/latest', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  let bodyObj = JSON.parse(body);
+
+
+  try {
+    for (const field in bodyObj.rates) {
+      if (currencies.hasOwnProperty(field) == true) {
+        currencies[field].rate = bodyObj.rates[field];
+      } else if (currencies.hasOwnProperty(field) == false) {
+        currencies[field] = {};
+        currencies[field].rate = bodyObj.rates[field];
+      }
+    }
+  }
+  // eslint-disable-next-line brace-style
+  catch (error) {
+    //console.log ("error");
+  }
+
+  //console.log(bodyObj);
+  // eslint-disable-next-line no-unused-vars
+
+
+
+}
+);
